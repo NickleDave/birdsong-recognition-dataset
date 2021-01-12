4305c6ea262061072ad86bba39bda35da5171661
David Nicholson <nickledave@users.noreply.github.com>
2021-01-12 09:35:32 -0500

    add __about__.py with package metadata, fix #5

---
+++ src/koumura/__about__.py
@@ -0,0 +1,37 @@
+import os.path
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__commit__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+
+try:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    base_dir = None
+
+__title__ = "koumura"
+__summary__ = "Functions for working with this data repository: https://figshare.com/articles/BirdsongRecognition/3470165"
+__uri__ = "https://github.com/NickleDave/koumura"
+
+__version__ = '0.1.1a1'
+
+if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
+    with open(os.path.join(base_dir, ".commit")) as fp:
+        __commit__ = fp.read().strip()
+else:
+    __commit__ = None
+
+__author__ = "David Nicholson"
+__email__ = "nicholdav@gmail.com"
+
+__license__ = "BSD"
+__copyright__ = "2018-present %s" % __author__
--- src/koumura/__init__.py
+++ src/koumura/__init__.py
@@ -1,3 +1,15 @@
+from .__about__ import (
+    __author__,
+    __commit__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
 from .koumura import parse_xml, load_song_annot, get_trans_mat
 from .koumura import Syllable, Sequence
 from .koumura import Resequencer
